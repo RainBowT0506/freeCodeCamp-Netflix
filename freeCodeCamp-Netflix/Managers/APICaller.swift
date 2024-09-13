d36e935c7a169ae3d319a4a56a6904592348d306
RainBowT0506 <rainbowt0506@gmail.com>
2024-09-13 15:07:19 +0800

    Querying database for individual movie
    feat: Implement search functionality with API integration
    
    - Added `searchWithQuery` function in `APIManager` for querying search results
    - Configured `UISearchController` in `SearchViewController` to handle search queries
    - Implemented `updateSearchResults` method to handle search query and update results
    - Added dynamic query handling to minimize unnecessary API calls
    - Set up `SearchResultsViewController` to display search results in a collection view
    - Configured collection view cells to display search results with movie posters
    - Made necessary properties public for data passing between controllers
    - Added error handling and logging for API requests

--- freeCodeCamp-Netflix/Managers/APICaller.swift
+++ freeCodeCamp-Netflix/Managers/APICaller.swift
@@ -156,4 +156,31 @@ class APICaller {
         }
         task.resume()
     }
+    
+
+    func search(with query: String, completion: @escaping (Result<[Title], Error>) -> Void) {
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else{
+            return
+        }
+        guard let url = URL(string: "\(Constants.BASE_URL)/3/search/movie?query=\(query)") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        // 將 API_KEY 放入 Authorization 標頭
+        request.setValue("Bearer \(Constants.API_KEY)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(APIError.failedTogetData))
+            }
+        }
+        task.resume()
     }
+}
