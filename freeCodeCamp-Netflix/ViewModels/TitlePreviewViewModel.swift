79db6540d64feeb5f65102dff9a0f4e1fe9a85e8
RainBowT0506 <rainbowt0506@gmail.com>
2024-09-17 01:26:28 +0800

    Creating TitlePreviewViewController
    feat: Implement TitlePreviewViewController with video playback and UI components
    
    - Added WKWebView for video playback
    - Created and styled title and overview labels
    - Implemented constraints for all UI elements
    - Integrated download button with corner radius and custom styles
    - Added TitlePreviewViewModel for passing title, overview, and video URL
    - Configured navigation from HomeViewController to TitlePreviewViewController
    - Fixed layout issues with overview label and refined UI

---
+++ freeCodeCamp-Netflix/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  freeCodeCamp-Netflix
+//
+//  Created by RainBowT on 2024/9/16.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel{
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
--- freeCodeCamp-Netflix/Views/CollectionViewTableViewCell.swift
+++ freeCodeCamp-Netflix/Views/CollectionViewTableViewCell.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
+protocol CollectionViewTableViewCellDelegate : AnyObject{
+    func collectionViewTableViewCellDidTapCell(_ cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel)
+}
+
 class CollectionViewTableViewCell: UITableViewCell {
     static let identifier = "CollectionViewTableViewCell"
+    
+    weak var delegate: CollectionViewTableViewCellDelegate?
     
     private var titles: [Title] = [Title]()
     
@@ -67,14 +73,23 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate,UICollectionView
         collectionView.deselectItem(at: indexPath, animated: true)
         
         let title = titles[indexPath.row]
-        guard let titleNmae = title.original_title ?? title.original_name else{
+        guard let titleName = title.original_title ?? title.original_name else{
             return
         }
         
-        APICaller.shared.getMovie(with: titleNmae + " trailer") { result in
+        APICaller.shared.getMovie(with: titleName + " trailer") { [weak self] result in
             switch result {
             case .success(let videoElement):
-                print(videoElement.id)
+                let title = self?.titles[indexPath.row]
+                guard let titleOverview = title?.overview else {
+                    return
+                }
+                guard let strongSelf = self else{
+                    return
+                }
+                
+                let viewModel = TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: titleOverview)
+                self?.delegate?.collectionViewTableViewCellDidTapCell(strongSelf, viewModel: viewModel)
             case .failure(let error):
                 print(error)
             }
